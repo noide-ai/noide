ecdf017ff63d256d0df3a2d7dce23a9495162e43
dtiourine <109561478+DTiourine@users.noreply.github.com>
2025-05-03 02:11:21 -0400

    Cleans up code for testing issue solver

--- src/services/ai/engine.py
+++ src/services/ai/engine.py
@@ -1,8 +1,8 @@
 from openai import OpenAI
 
-from src.services.ai._utils import generate_prompt
+from src.services.ai.prompts import generate_prompt
 from ...models import Issue, File, FileList
-from .test_objects import test_issue, test_file_list
+
 
 class IssueSolver:
     _openai_api_key: str | None = None
@@ -26,18 +26,3 @@ class IssueSolver:
         res = response.output_parsed
 
         return res
-
-
-if __name__ == '__main__':
-    issue_solver = IssueSolver()
-
-    issue = Issue(title="Issue Title", body="Issue Body")
-    file1 = File(path="file1.py", content="Hello World")
-    file2 = File(path="fil2.py", content="Hello World 2")
-
-    file_list = [file1, file2]
-
-    # res = issue_solver.generate_response(file_list)
-    # print(res)
-    test_response = issue_solver.solve_issues(test_issue, test_file_list)
-    print(test_response)
